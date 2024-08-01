295dd70612db768fee623494739957fcbb13af52
dannycod3r <oseid7807@gmail.com>
2024-08-01 23:21:35 +0000

    Add function docs

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,4 +5,5 @@ from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """return values with the appropriate types"""
     return [(i, len(i)) for i in lst]
