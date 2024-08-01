45d82feda8aa25ae5923d7d4c68de6ee38d1cce8
dannycod3r <oseid7807@gmail.com>
2024-08-01 19:43:07 +0000

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module contains function that returns the string
+   version of float
+"""
+
+
+def to_str(n: float) -> str:
+    """Return the string of n"""
+    return str(n)
