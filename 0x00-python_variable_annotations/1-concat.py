580dc24ecd354ea6339208b4556526db8e2f01cc
dannycod3r <oseid7807@gmail.com>
2024-08-01 19:21:42 +0000

    Task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module contains function to concatenate two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns str1 + str2"""
+    return (str1 + str2)
