82fb674be67b04cdaf7f5de6acbe468b61e7942f
dannycod3r <oseid7807@gmail.com>
2024-08-30 20:11:00 +0000

    --no-edit

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -14,10 +14,10 @@ def measure_time(n: int, max_delay: int) -> float:
     average time per task.
     """
     start_time = time.time()
-    
+
     asyncio.run(wait_n(n, max_delay))
-    
+
     end_time = time.time()
     total_time = end_time - start_time
-    
+
     return total_time / n
