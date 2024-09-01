3ea53857af326f2dd73409bf9b3b5cdad2e6a184
dannycod3r <oseid7807@gmail.com>
2024-09-01 10:14:23 +0000

    task 1: return async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Supplies a function that returns async comprehenshion
+"""
+
+import asyncio
+from typing import Generator
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[float, None, None]:
+    """
+    collect 10 random numbers using an async comprehensing
+    over async_generator, then return the 10 random numbers.
+    """
+    result = [num async for num in async_generator()]
+    return result
